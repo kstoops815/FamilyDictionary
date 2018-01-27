4eb5a4eb52bd907bef23ad4f5518afde2126ddae
Kelly Stoops <kstoops815@gmail.com>
2018-01-27 12:20:06 -0600

    exercise complete

--- FamilyDictionary/Program.cs
+++ FamilyDictionary/Program.cs
@@ -10,6 +10,32 @@ namespace FamilyDictionary
     {
         static void Main(string[] args)
         {
+            Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+
+            myFamily.Add("sister", new Dictionary<string, string>()
+            {
+                {"name", "Cayce"},
+                {"age", "41"}
+            });
+            myFamily.Add("firstBorn", new Dictionary<string, string>(){
+                { "name", "Maximus"},
+                { "age", "15"}
+            });
+            myFamily.Add("secondBorn", new Dictionary<string, string>(){
+                { "name", "Matthias"},
+                { "age", "13"}
+            });
+            myFamily.Add("dog", new Dictionary<string, string>(){
+                { "name", "Eleanor"},
+                { "age", "7"}
+            });
+
+            foreach (var item in myFamily)
+            {
+                Console.WriteLine($"{item.Value["name"]} is my {item.Key}.");
+            }
+
+            Console.Read();
         }
     }
 }
